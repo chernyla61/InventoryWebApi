a233a843d0943b822f55d8505fda435701b5c356
User <user@hackerrank.workspace>
2024-07-18 20:28:06 +0000

    Add initial repository
    Commit by HackerRank

---
+++ InventoryWebApi/Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using InventoryWebApi.DataAccess;
+using InventoryWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using InventoryWebApi.DTO;
+using InventoryWebApi.Services;
+
+namespace InventoryWebApi.Controllers
+{
+    [ApiController]
+    [Route("inventory/item")]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IInventoryService _inventoryService;
+
+        public InventoryController(IInventoryService inventoryService)
+        {
+            _inventoryService = inventoryService;
+        }
+        
+    }
+
+}
---
+++ InventoryWebApi/Services/InventoryService.cs
@@ -0,0 +1,47 @@
+﻿using InventoryWebApi.DataAccess;
+using InventoryWebApi.DTO;
+using InventoryWebApi.Models;
+
+namespace InventoryWebApi.Services
+{
+    public class InventoryService : IInventoryService
+    {
+        private readonly IRepository _repository;
+
+        public InventoryService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<InventoryItem> CreateItem(CreateForm createForm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteItem(int barcode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<InventoryItem>> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<InventoryItem>> GetItemsByQuery(ItemQueryModel queryParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<InventoryItem>> GetSortedItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateItem(int barcode, CreateForm updateForm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
